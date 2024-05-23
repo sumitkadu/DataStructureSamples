c884fa363deb86ec84afb396eee821255b82a61e
Sumit Kadu <sumit.kadu@live.com>
2024-05-23 17:32:42 -0400

    added dijkstra example

--- Program.cs
+++ Program.cs
@@ -18,7 +18,9 @@
 
             //StackExample();
 
-            QueueExample();
+            //QueueExample();
+
+            //DijkstraExample();
             Console.WriteLine("Press any key to exit...");
             Console.ReadLine();
         }
@@ -157,6 +159,21 @@
             Console.WriteLine("\nOne item removed from queue");
             myQueue.Dequeue();
             myQueue.PrintQueue();
+        }
+
+        private static void DijkstraExample()
+        {
+            int[, ] graph = new int[, ] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+									{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+									{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+									{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+									{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+									{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+									{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+									{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+									{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+		    GFG t = new();
+            GFG.Dijkstra(graph, 4);
         }
     }   
 }
